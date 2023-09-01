c5abeacc6e189758f19172afa3d7d1829d4b259f
Thiago César oliveira <thiafa.vos@gmail.com>
2023-09-01 09:50:01 -0300

    Feat: additing fields in User

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -20,8 +20,9 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'last_name' => fake()->lastName(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
     }
